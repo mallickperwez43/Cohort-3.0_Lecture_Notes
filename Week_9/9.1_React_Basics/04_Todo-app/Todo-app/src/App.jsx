5379e6038542ed13e3747905702eb132f4bf1137
Perwez Mallick <mallickperwez43@gmail.com>
2026-04-25 22:33:52 +0530

    Added a simple Todo App in React

---
+++ Week_9/9.1_React_Basics/04_Todo-app/Todo-app/src/App.jsx
@@ -0,0 +1,39 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "Go to gym",
+      desc: "Hit the gym regurarly",
+      done: false
+    },
+  ]);
+
+  function addTodo() {
+    let newArray = [];
+    for (let i = 0; i < todos.length; i++) {
+      newArray.push(todos[i]);
+    }
+    newArray.push({
+      title: document.getElementById('title').value,
+      desc: document.getElementById('desc').value,
+      done: true
+    });
+    setTodos(newArray);
+  }
+
+  return (
+    <div>
+      <input id='title' type="text" placeholder='Title'></input>
+      <input id='desc' type="text" placeholder='Desc'></input>
+      <br />
+      <button onClick={addTodo}>Add Todo</button>
+      <br />
+      {JSON.stringify(todos)}
+    </div>
+  )
+}
+
+export default App
