0292271585a27ed1c8946b62951db35f4a6fb1a3
Perwez Mallick <mallickperwez43@gmail.com>
2026-04-29 22:22:34 +0530

    Added Conditonal Rendering in REact

---
+++ Week_9/9.3_React_Offline/03_Conditional-Rendering/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ToggleMessage from './components/ToggleMessage'
+
+function App() {
+  return (
+    <div>
+      <ToggleMessage />
+    </div>
+  )
+}
+
+export default App
