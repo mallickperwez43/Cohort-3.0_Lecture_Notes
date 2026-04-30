040a47115189a4cf1d9f56c4fe5ff5961c3674e2
Perwez Mallick <mallickperwez43@gmail.com>
2026-04-30 16:55:51 +0530

    Updated the share validator to return hash

--- Week_15/Brain-app/Backend/src/validators/contentValidators.ts
+++ Week_15/Brain-app/Backend/src/validators/contentValidators.ts
@@ -9,22 +9,15 @@ export const createContentSchema = z.object({
     tags: z.array(z.string()).default([]),
 });
 
-// 3. Define the Update Schema (remains partial)
 export const updateContentSchema = createContentSchema.partial();
 
-// 4. Define the Share Schema
 export const shareLinkSchema = z.object({
-    share: z.boolean({
+    share: z.coerce.boolean({
         message: "Share status is required"
     })
 });
 
-/**
- * TypeScript Type Inference
- * These lines export the TS types based on your schemas.
- * Now you can use 'CreateContentInput' in your controller 
- * and it will stay perfectly in sync with your validation rules.
- */
+
 export type CreateContentInput = z.infer<typeof createContentSchema>;
 export type UpdateContentInput = z.infer<typeof updateContentSchema>;
 export type ShareLinkInput = z.infer<typeof shareLinkSchema>;
