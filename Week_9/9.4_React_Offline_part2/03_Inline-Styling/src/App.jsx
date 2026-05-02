c4e8ccde588ef84f44666cf9fd26285c4ab309d9
Perwez Mallick <mallickperwez43@gmail.com>
2026-05-02 22:29:29 +0530

    Added Inline Styling in React

---
+++ Week_9/9.4_React_Offline_part2/03_Inline-Styling/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+function App() {
+
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ Week_9/9.4_React_Offline_part2/03_Inline-Styling/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyComponent = () => {
+    return (
+        <div style={{ backgroundColor: 'blue', color: 'white', padding: 10, borderRadius: 20, textAlign: "center" }}>
+            Hello, World!
+        </div>
+    )
+}
+
+export default MyComponent
